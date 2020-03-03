59b7a769a39282c2aba0f20d591af9d64288642c
Riyachoudhary25 <61678297+Riyachoudhary25@users.noreply.github.com>
2020-03-03 20:10:10 +0530

    Create bitwiseoperator.c

---
+++ bitwiseoperator.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+	int a=27,b=39;
+	printf("\n a&b=%d",a&b);
+	printf("\n a/b=%d",a|b);
+	printf("\n ~a=%d",~a);
+	printf("\n ~b=%d",~b);
+	printf("\n a^b=%d",a^b);
+	printf("\n a<<1=%d",a<<1);
+	printf("|n b>>1=%d",b>>1);
+}
