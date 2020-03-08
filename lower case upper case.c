4fc854440f8bf57cf391b393c6a3dee86a1f9009
Riyachoudhary25 <61678297+Riyachoudhary25@users.noreply.github.com>
2020-03-08 19:04:30 +0530

    Create lower case upper case.c

---
+++ lower case upper case.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	char ch;
+	printf("\n Enter any character:");
+	scanf("%c",&ch);
+	if(ch>='A'&&ch<='Z');
+	printf("\n The entered character was in upper case. In lower case it is:%c",(ch+32));
+	printf("\n The entered character was in lower case. In upper case it is:%c",(ch-32))
+	return 0;
+}
